47f86d23298c0b76973370eef6c36a4d9e898bc2
frecamposo <fre.camposo@profesor.duoc.cl>
2024-12-05 22:28:21 -0300

    ok

--- eGESVEN/settings.py
+++ eGESVEN/settings.py
@@ -75,12 +75,20 @@ WSGI_APPLICATION = 'eGESVEN.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'orcl',
+        'USER': 'eGESVEN',
+        'PASSWORD': 'eGESVEN',
+        'HOST': 'localhost',
+        'PORT': 1522,
     }
 }
-
+# 'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
